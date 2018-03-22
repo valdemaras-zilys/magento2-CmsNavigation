147b45611e8811508225ca60cdf76ba8c37ca1aa
Valdemaras Zilys <valdemaras.zilys@gmail.com>
2018-03-22 00:01:42 +0000

    formatting change

--- Plugin/Block/Navigation.php
+++ Plugin/Block/Navigation.php
@@ -1,10 +1,11 @@
-<?php 
+<?php
 
 namespace Raguvis\CmsNavigation\Plugin\Block;
 
 use Magento\Framework\Data\Tree\NodeFactory;
 use Magento\Cms\Model\ResourceModel\Page\CollectionFactory as PageCollectionFactory;
 use Magento\Framework\ObjectManagerInterface as Objectmanager;
+
 class Navigation
 {
     /**
@@ -36,7 +37,8 @@ class Navigation
         NodeFactory $nodeFactory,
         PageCollectionFactory $pageCollectionFactory,
         Objectmanager $objectmanager
-    ) {
+    )
+    {
         $this->nodeFactory = $nodeFactory;
         $this->pageCollectionFactory = $pageCollectionFactory;
         $this->objectmanager = $objectmanager;
@@ -58,20 +60,22 @@ class Navigation
         $outermostClass = '',
         $childrenWrapClass = '',
         $limit = 0
-    ) {
+    )
+    {
         $pageCollection = $this->getPageCollection();
-        foreach($pageCollection as $page){
+        foreach ($pageCollection as $page) {
             $pageArray = $this->getNodeAsArray($page);
-            $this->addNode($subject,$pageArray);
-        } 
+            $this->addNode($subject, $pageArray);
+        }
     }
 
     /**
      * Prepare and return array of pages to be included in top navigation
-     * 
+     *
      * @return Magento\Cms\Model\ResourceModel\Page\Collection
      */
-     protected function getPageCollection(){
+    protected function getPageCollection()
+    {
         $collection = $this->pageCollectionFactory->create();
         $collection->addFieldToFilter('show_in_navigation', ['eq' => 1]);
         $collection->addFieldToFilter('is_active', ['eq' => 1]);
@@ -84,25 +88,26 @@ class Navigation
      * @param Magento\Cms\Model\Page $page
      * @return array
      */
-     protected function getNodeAsArray($page)
-     {
-         return [
-             'name' => __($page->getTitle()),
-             'id' => $page->getId(),
-             'url' => $this->cmsPageHelper->getPageUrl($page->getId()),
-             'has_active' => false,
-             'is_active' => ($page->getIdentifier() == $this->currentPageIdentifier)
-         ];
-     }
+    protected function getNodeAsArray($page)
+    {
+        return [
+            'name' => __($page->getTitle()),
+            'id' => $page->getId(),
+            'url' => $this->cmsPageHelper->getPageUrl($page->getId()),
+            'has_active' => false,
+            'is_active' => ($page->getIdentifier() == $this->currentPageIdentifier)
+        ];
+    }
 
     /**
      * Add navigation node witch page details
-     * 
+     *
      *
      * @param \Magento\Theme\Block\Html\Topmenu $subject
      * @param array $pageArray
      */
-    protected function addNode($subject, $pageArray){
+    protected function addNode($subject, $pageArray)
+    {
         $node = $this->nodeFactory->create(
             [
                 'data' => $pageArray,
--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -2,10 +2,13 @@
 /**
  * Raguvis CmsNavigation
  */
+
 namespace Raguvis\CmsNavigation\Setup;
+
 use Magento\Framework\Setup\InstallSchemaInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
+
 class InstallSchema implements InstallSchemaInterface
 {
     /**
